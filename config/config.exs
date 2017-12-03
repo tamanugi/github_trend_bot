44b8b93ef83f88d7c913c86877dff25001e1090b
tamanugi <tamanugi@gmail.com>
2017-12-03 22:08:42 +0900

    config for scheduling

--- config/config.exs
+++ config/config.exs
@@ -8,23 +8,7 @@ use Mix.Config
 # if you want to provide default values for your application for
 # 3rd-party users, it should be done in your "mix.exs" file.
 
-# You can configure your application as:
-#
-#     config :github_trend_bot, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:github_trend_bot, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :quantum, GithubTrendBot.Scheduler,
+  jobs: [
+    {"0 * * * *", {GithubTrendBot, :post_trend, []}}
+  ]
